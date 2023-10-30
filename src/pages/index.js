94b745b804615bbd70498552de74e5fa49aba679
Howen <howen.huang@qq.com>
2023-10-30 19:33:13 +0800

    添加移动端判断 使用移动端图片作为主页加载

--- src/pages/index.js
+++ src/pages/index.js
@@ -1,9 +1,41 @@
 import React, { useState, useEffect } from 'react';
 import { useHistory } from '@docusaurus/router';
 
+function useDeviceType() {
+  const [device, setDevice] = React.useState('desktop'); // 默认为 desktop
+
+  React.useEffect(() => {
+    // 使用 matchMedia 判断设备类型
+    const setResponsiveness = () => {
+      if (window.innerWidth < 768) {
+        setDevice('mobile');
+      } else if (window.innerWidth < 992) {
+        setDevice('tablet');
+      } else {
+        setDevice('desktop');
+      }
+    };
+
+    setResponsiveness();
+    window.addEventListener("resize", () => setResponsiveness());
+
+    return () => {
+      window.removeEventListener("resize", () => setResponsiveness());
+    };
+  }, []);
+
+  return device;
+}
+
+
 function Home() {
+  const device = useDeviceType();
   const history = useHistory();
   const [fade, setFade] = useState(false);
+  let imagePath = "/img/genshin start.jpg"; // 默认为 desktop 图片
+  if (device === 'mobile') {
+    imagePath = "/img/genshin start phone.png"; // 手机图片路径
+  }
 
   useEffect(() => {
     // 设置一个计时器，2秒后开始透明度渐变
@@ -27,7 +59,7 @@ function Home() {
       style={{ width: '100%', height: '100vh', display: 'flex', justifyContent: 'center', alignItems: 'center' }}
       className={fade ? 'fade-out' : ''}
     >
-      <img src="/img/genshin start.jpg" alt="Welcome" style={{ width: '100%', height: '100vh' }}/>
+      <img src={imagePath} alt="Welcome" style={{ width: '100%', height: '100vh' }}/>
     </div>
   );
 }
