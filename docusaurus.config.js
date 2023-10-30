3c8c43f294ae906f72d04a0f5a7abdee06156b2f
Howen <howen.huang@qq.com>
2023-10-30 21:43:13 +0800

    update

--- docusaurus.config.js
+++ docusaurus.config.js
@@ -6,7 +6,7 @@ const darkCodeTheme = require('prism-react-renderer/themes/dracula');
 
 /** @type {import('@docusaurus/types').Config} */
 const config = {
-  title: ' ',
+  title: 'Genshin College Alliance',
   tagline: 'Dinosaurs are cool',
   url: 'https://your-docusaurus-test-site.com',
   baseUrl: '/',
